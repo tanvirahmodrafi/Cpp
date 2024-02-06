7e3db5a5e427bd45002999b50541e68d752805b4
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2024-02-06 07:57:47 +0600

    update

--- new.cpp
+++ new.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    int arr[5] = {1,8,3,0,6};
-    int n = 5;
-    // for (int i = 1; i < n; i++)
-    // {
-    //     int j = i - 1;
-    //     int current = arr[i];
-    //     while (j >= 0 && arr[j] > current)
-    //     {
-    //        arr[j+1]=arr[j];
-    //        j--;
-    //     }
-    //     arr[j+1] = current;
-    // }
-    for (int i = 1; i < n; i++)
-    {
-        int j = i -1;
-        int current = arr[i];
-        while(j >= 0 && arr[j] > current){
-            arr[j+1]=arr[j];
-            j--;
-        }
-        arr[j+1] = current;
+struct Dragon {
+    int s;
+    int b;
+    bool operator < (const Dragon& other) const {
+        return s < other.s;
     }
-    
+};
+void main() {
+    int s, n;
+    cin >> s >> n;
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i]<<' ';
+    vector<Dragon> dragons(n);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> dragons[i].s >> dragons[i].b;
     }
     
-    return 0;
+    sort(dragons.begin(), dragons.end());
+    
+    for (int i = 0; i < n; i++) {
+        if (s > dragons[i].s) {
+            s += dragons[i].b;
+        } else {
+            cout << "NO" << endl;
+            return ;
+        }
+    }
+
+    cout << "YES" << endl;
+
+    return ;
 }
