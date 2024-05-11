eb0df75e015f76931fdc05026a954f6bf6cc76e5
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2024-05-11 13:31:56 +0600

    update

--- NCPC.cpp
+++ NCPC.cpp
@@ -1,95 +1,31 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-struct ListNode
+int longestPalindromeSubseq(string s)
 {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-ListNode* insertElement(ListNode *&head, int val)
-{
-    ListNode *newNode = new ListNode(val);
-
-    if (head == nullptr)
+    sort(s.begin(), s.end());
+    int j = 1, m = 0;
+    for (int i = 0; i < s.length(); i++)
     {
-        head = newNode;
-        return head;
+        if (i + 1 == s.length() || s[i] != s[i + 1])
+        {
+            m = max(j, m);
+            j = 1;
+        }
+        else
+        {
+            j++;
+        }
     }
 
-    ListNode *current = head;
-    while (current->next != nullptr)
-    {
-        current = current->next;
-    }
-    current->next = newNode;
-
-    return head;
-}
-
-ListNode *insertAtHead(ListNode *head, int val){
-    ListNode *newNode = new ListNode(val);
-
-    newNode->next = head;
-    head = newNode;
-
-    return head;
-}
- ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode* list1 = NULL;
-    ListNode* list2 = NULL;
-    ListNode* head = NULL;
-    while (l1 != NULL) {
-        list1 = insertAtHead(list1, l1->val);
-        l1 = l1->next;
-    }
-
-    while (l2 != NULL) {
-        list2 = insertAtHead(list2, l2->val);
-        l2 = l2->next;
-    }
-
-    bool carry = false;
-    int n;
-
-    while(list1 != NULL || list2 != NULL || carry) {
-        int n = (list1 ? list1->val : 0) + (list2 ? list2->val : 0) + carry;
-        carry = (n >= 10);
-        n %= 10;
-        head = insertElement(head, n);
-
-        if (list1) list1 = list1->next;
-        if (list2) list2 = list2->next;
-    }
-
-    ListNode* current1 = head;
-    while (current1 != NULL) {
-        cout << current1->val << ' ';
-        current1 = current1->next;
-    }
-    return head;
+    return m;
 }
 
 int main()
 {
-    ListNode *head =NULL;
-    ListNode *head2 = NULL;
+    string n = "bbbbaabbb";
 
+    cout << n << endl;
 
-    // insertElement(head,9);
-    // insertElement(head,9);
-    // insertElement(head,9);
-    // insertElement(head,9);
-    // insertElement(head,9);
-    // insertElement(head,9);
-    // insertElement(head,9);
-    // insertElement(head2,9);
-    // insertElement(head2,9);
-    // insertElement(head2,9);
-    // insertElement(head2,9);
-    // addTwoNumbers(head,head2);
-    cout<<0/0;
-    
-
+    return 0;
 }
