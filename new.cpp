7147e1a6d52388f83bd9040de66588efe67d431c
tanvirahmodrafi <87052044+tanvirahmodrafi@users.noreply.github.com>
2025-01-09 08:26:56 +0600

    up

--- new.cpp
+++ new.cpp
@@ -1,56 +1,27 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-vector<int> findGridDimensions(int k, vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    vector<int> result;
+struct student{
+    int age;
+    string name;
+};
 
-    // Try all divisors of k to find valid grid dimensions
-    for (int n = 1; n * n <= k; ++n) {
-        if (k % n == 0) {
-            int m = k / n;
-
-            
-            bool isValid = true;
-            for (int i = 0; i < n; ++i) { // for each row
-                for (int j = 1; j < m; ++j) { // for each column in the row
-                    if (nums[i * m + j] < nums[i * m + j - 1]) { // check if sorted
-                        isValid = false;
-                        break;
-                    }
-                }
-                if (!isValid) {
-                    break;
-                }
-            }
-
-            if (isValid) {
-                result.push_back(n);
-                result.push_back(m);
-                return result;
-            }
-        }
-    }
-
-    return result; // No valid grid found
-}
 
 int main() {
-    int t;
-    cin >> t;
+    
+    student s1;
 
-    while (t--) {
-        int k;
-        cin >> k;
+    int a ;
+    char buf;
+    string str;
+    str = "123";
+    char data[19];
 
-        vector<int> nums(k);
-        for (int i = 0; i < k; ++i) {
-            cin >> nums[i];
-        }
+    s1.age = 10;
+    s1.name = "Rafi";
 
-        vector<int> dimensions = findGridDimensions(k, nums);
-        cout << dimensions[0] << " " << dimensions[1] << endl;
-    }
+    cout << s1.name << endl;
 
     return 0;
 }
